21e13ecae7652c8d3fb99aff7cb3af5def772502
Thomas MICHEL-WEIL <tmichel-@e1r7p17.clusters.42paris.fr>
2023-02-15 17:54:03 +0100

    01

--- srcs/philo/init.c
+++ srcs/philo/init.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.c                                            :+:      :+:    :+:   */
+/*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmichel- <tmichel-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 16:33:44 by tmichel-          #+#    #+#             */
-/*   Updated: 2023/02/14 18:11:10 by tmichel-         ###   ########.fr       */
+/*   Updated: 2023/02/15 17:49:53 by tmichel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ void	ft_number(int ac, char **av)
 					j++;
 			}
 			else
-				ft_error("Parsing error #1\n");
+				ft_error(DIGITS);
 		}
 		j = 0;
 	}
@@ -52,24 +52,55 @@ int	parse_args(int ac, char **av)
 	int	i;
 
 	i = 0;
-	if (ac != 6)
-		ft_error("Parsing error #2\n");
+	if (ac < 5 && ac > 6)
+		ft_error(NUMB_ARG);
 	ft_number(ac, av);
 	if (ft_atoi(av[1]) < 2)
-		ft_error("Parsing error #3\n");
+		ft_error(ONE_PHILO);
 	if (ft_atoi(av[2]) <= ft_atoi(av[3]) + ft_atoi(av[4]))
-		ft_error("Parsing error #4\n");
-	if (ft_atoi(av[2]) <= 0 || ft_atoi(av[3]) <= 0 || ft_atoi(av[4]) <= 0
-		|| ft_atoi(av[5]) <= 0)
-		ft_error("Parsing error #5\n");
+		ft_error(T_DIE);
+	if (ft_atoi(av[2]) <= 0 || ft_atoi(av[3]) <= 0 || ft_atoi(av[4]) <= 0)
+		ft_error(T_ACTION);
+	if (ac == 6 && ft_atoi(av[5]) <= 0)
+		ft_error(T_MEALS);
 	return (1);
+}
+
+void	ft_init(int ac, char **av, t_info *info)
+{
+	int	i;
+
+	i = -1;
+	if (parse_args(ac, av) == 1)
+	{
+		info->n_philo = ft_atoi(av[1]);
+		info->t_die = ft_atoi(av[2]);
+		info->t_eat = ft_atoi(av[3]);
+		info->t_sleep = ft_atoi(av[4]);
+		info->philo = malloc(sizeof(t_philo) * info->n_philo);
+		if (!info->philo)
+			ft_error(MLC);
+		while (++i < info->n_philo)
+		{
+			info->philo[i].id = i;
+			info->philo[i].l_fork_id = i;
+			if (info->philo[i].id == 0)
+				info->philo[i].r_fork_id = info->n_philo - 1;
+			else
+				info->philo[i].r_fork_id = info->philo[i].id - 1;
+		}
+	}
 }
 
 int	main(int ac, char **av)
 {
-	int	i;
+	int		i;
+	t_info	info;
 
-	i = parse_args(ac, av);
-	printf("parsing result is %d\n", i);
+	i = -1;
+	ft_init(ac, av, &info);
+	printf("il y a %d philos\n", info.n_philo);
+	while (++i < info.n_philo)
+		printf("p_id = %d || lf_id = %d || rf_id = %d\n", info.philo[i].id, info.philo[i].l_fork_id, info.philo[i].r_fork_id);
 	return (0);
 }
